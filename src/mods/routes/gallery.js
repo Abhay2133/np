bdb8a522b03bbaf25fbec7f8dcb5e55aad45708a
abhay2132 <abhaybro768@gmail.com>
2021-12-08 10:13:44 +0530

    handlebars /path bug fixed !

--- src/mods/routes/gallery.js
+++ src/mods/routes/gallery.js
@@ -2,7 +2,9 @@ const fs = require("fs"),
 	hlpr = require("../hlpr")
 
 module.exports = async (req, res) => {
-	let photos = await hlpr.readir(j(sdir, "public", "imgs")),
-		photosrc = photos.map((img) => j("imgs", img));
+	log(req.method, req.url);
+	let photos = await hlpr.readir(j(sdir, "public", "gallery")),
+		photosrc = photos.map((img) => j("/gallery", img));
+	
 	res.render("gallery", { photos : photosrc });
 }
